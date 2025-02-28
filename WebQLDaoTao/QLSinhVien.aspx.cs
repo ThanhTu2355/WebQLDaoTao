965073b3a28bb524694530935fe98396f6e07523
nguyentu1112005 <nguyentu1112005@gmail.com>
2025-02-28 11:14:42 +0700

    QLSinhVien

--- WebQLDaoTao/QLSinhVien.aspx.cs
+++ WebQLDaoTao/QLSinhVien.aspx.cs
@@ -4,14 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebQLDaoTao.Models;
 
 namespace WebQLDaoTao
 {
     public partial class QLSinhVien : System.Web.UI.Page
     {
+        SinhVienDAO svDAO = new SinhVienDAO();
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void btnLuu_Click(object sender, EventArgs e)
+        {
+            //lấy thông tin từ form
+            string masv = txtMaSV.Text;
+            string hosv = txtHoSV.Text;
+            string tensv = txtTenSV.Text;
+            bool gioitinh = rdNam.Checked ? true : false;
+            DateTime ngaysinh = DateTime.Parse(txtNgaySinh.Text);
+            string noisinh = txtNoiSinh.Text;
+            string diachi = txtDiaChi.Text;
+            string makh = ddlKhoa.SelectedValue;
+
+            SinhVien sv = new SinhVien()
+            {
+                MaSV = masv,
+                HoSV = hosv,
+                TenSV = tensv,
+                GioiTinh = gioitinh,
+                NgaySinh = ngaysinh,
+                NoiSinh = noisinh,
+                DiaChi = diachi,
+                MaKH = makh,
+            };
+
+            svDAO.Insert(sv);
+            gvSinhVien.DataBind();
         }
     }
 }
